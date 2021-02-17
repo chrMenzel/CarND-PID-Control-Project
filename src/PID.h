1de1b463cbb6ea51ecde9412e212b694025893c7
Christian Menzel <info@chrmenzel.de>
2021-02-17 19:44:22 +0100

    implement functioning pid conroller

--- src/PID.h
+++ src/PID.h
@@ -2,49 +2,48 @@
 #define PID_H
 
 class PID {
- public:
-  /**
-   * Constructor
-   */
+public:
+  /*
+  * Errors
+  */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /*
+  * Coefficients
+  */ 
+  double Kp;
+  double Ki;
+  double Kd;
+
+  /*
+  * Constructor
+  */
   PID();
 
-  /**
-   * Destructor.
-   */
+  /*
+  * Destructor.
+  */
   virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
+  /*
+  * Initialize PID.
+  * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+  */
   void Init(double Kp_, double Ki_, double Kd_);
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
+  /*
+  * Update the PID error variables given cross track error.
+  * @param cte The current cross track error
+  */
   void UpdateError(double cte);
-
+  
   /**
    * Calculate the total PID error.
    * @output The total PID error
    */
   double TotalError();
-
- private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
-
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
 };
 
-#endif  // PID_H
+#endif /* PID_H */
--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,10 @@ string hasData(string s) {
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.find_last_of("]");
-  if (found_null != string::npos) {
+  if (found_null != std::string::npos) {
     return "";
   }
-  else if (b1 != string::npos && b2 != string::npos) {
+  else if (b1 != std::string::npos && b2 != std::string::npos) {
     return s.substr(b1, b2 - b1 + 1);
   }
   return "";
@@ -33,18 +33,20 @@ string hasData(string s) {
 int main() {
   uWS::Hub h;
 
-  PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
+  double Kp = 0.1;
+  double Ki = 0.001;
+  double Kd = 2.0;
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
-                     uWS::OpCode opCode) {
+  PID pid;
+  pid.Init(Kp, Ki, Kd);
+
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
-      auto s = hasData(string(data).substr(0, length));
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
+      auto s = hasData(std::string(data).substr(0, length));
 
       if (s != "") {
         auto j = json::parse(s);
@@ -53,20 +55,13 @@ int main() {
 
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<string>());
-          double speed = std::stod(j[1]["speed"].get<string>());
-          double angle = std::stod(j[1]["steering_angle"].get<string>());
-          double steer_value;
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
+          double cte = std::stod(j[1]["cte"].get<std::string>());
           
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+          pid.UpdateError(cte);
+          
+          double steer_value = pid.TotalError();
+          
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
@@ -74,10 +69,10 @@ int main() {
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }  // end "telemetry" if
+        }
       } else {
         // Manual driving
-        string msg = "42[\"manual\",{}]";
+        std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
     }  // end websocket message if
